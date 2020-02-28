ad960e9606145743c71578316b7968b170ed2209
Rainweic <861514706@qq.com>
2020-02-28 20:56:14 +0800

    [update] 更新预处理

--- train/train.py
+++ train/train.py
@@ -28,8 +28,8 @@ net.hybridize()
 transform_train = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.RandomFlipLeftRight(),
-    transforms.RandomContrast(),
-    transforms.RandomBrightness(),
+    transforms.RandomContrast(0.8),
+    transforms.RandomBrightness(0.8),
     transforms.ToTensor(),
     transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
 ])
