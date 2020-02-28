d390ce14ae1cc28ce31f97df6300aede2e451241
Rainweic <861514706@qq.com>
2020-02-28 20:44:06 +0800

    [update] add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="maskdet",
+    version=0.1,
+    author="rainweic",
+    url="None",
+    description="mask det",
+    packages=find_packages(exclude=("model", "train")),
+    python_requires=">=3.5",
+    install_requires=[
+        "mxnet",
+        "opencv-python"
+    ],
+)
